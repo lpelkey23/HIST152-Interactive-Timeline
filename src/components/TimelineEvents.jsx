c35884bce953cd1f3116ac5d1141cef39941851c
Logan Pelkey <lepelkey@iu.edu>
2025-04-04 20:02:45 -0400

    extracted events into separate file

---
+++ src/components/TimelineEvents.jsx
@@ -0,0 +1,21 @@
+export const exampleEvents = {
+  1969: [
+    {
+      title: 'Apollo 11 Moon Landing',
+      type: 'video',
+      src: 'https://www.youtube.com/embed/cwZb2mqId0A',
+    },
+    {
+      title: 'Woodstock Music Festival',
+      type: 'image',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Woodstock_redmond_stage.JPG/800px-Woodstock_redmond_stage.JPG',
+    },
+  ],
+  1963: [
+    {
+      title: 'March on Washington',
+      type: 'video',
+      src: 'https://www.youtube.com/embed/I47Y6VHc3Ms',
+    },
+  ],
+};
--- src/components/YearSelector.jsx
+++ src/components/YearSelector.jsx
@@ -1,28 +1,7 @@
 // components/YearSelector.jsx
 
 import Button from './Button';
-
-const exampleEvents = {
-  1969: [
-    {
-      title: 'Apollo 11 Moon Landing',
-      type: 'video',
-      src: 'https://www.youtube.com/embed/cwZb2mqId0A',
-    },
-    {
-      title: 'Woodstock Music Festival',
-      type: 'image',
-      src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Woodstock_redmond_stage.JPG/800px-Woodstock_redmond_stage.JPG',
-    },
-  ],
-  1963: [
-    {
-      title: 'March on Washington',
-      type: 'video',
-      src: 'https://www.youtube.com/embed/I47Y6VHc3Ms',
-    },
-  ],
-};
+import { exampleEvents } from './TimelineEvents';
 
 export default function YearSelector({ decade, onEventSelect }) {
   const years = Array.from({ length: 10 }, (_, i) => decade + i);
